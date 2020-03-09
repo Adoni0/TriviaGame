66c97eb422ec63b8ddcc2bcb0a92e07d5bd1c4a2
Adam SOudani <asoudani@email.com>
2020-03-08 21:42:17 -0700

    Initial site files

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//global variables
+var questionAsked = [];
+var answerChoice = [];
+var timeRemaining = '';
+//need a start game function that is called later in the code
+//set time interval for 30 seconds per question
+//variable for questions asked
+//variable for answer choices
+//have new screen appear showing correct answer with image
+
+
+//Q1: Lemurs are native to only one country on earth, name that country. answer-Madagascar
+//Q2: how many hearts does an octopus have? answer-3
+//Q3: What colour are flamingos when they are born? answer-grey
+//Q4: Which animal is also known as a "Sand Rat" or "Desert Kangaroo"? answer -gerbil
+//Q5: The horn of a rhinoceros is made of which material? answer-keratin
+//Q6: How many knees does an elephant have? answer-4

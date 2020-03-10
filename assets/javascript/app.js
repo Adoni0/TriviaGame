d9b3b39c517aaf8ac5cc600f2415d5d186b5c05f
Adam SOudani <asoudani@email.com>
2020-03-09 19:30:31 -0700

    Initial site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,33 @@
 var questionAsked = [];
 var answerChoice = [];
 var timeRemaining = '';
+var correctAnswer = 0;
+var wrongAnswer = 0;
+
 //need a start game function that is called later in the code
+//start game screen, on button click run startGame function  and go to different page
 //set time interval for 30 seconds per question
 //variable for questions asked
 //variable for answer choices
 //have new screen appear showing correct answer with image
-
+//final screen showing number of correct and incorrect answers and option to restart the game
+//start over button on last page, resets the game (using a resetGame function)
 
 //Q1: Lemurs are native to only one country on earth, name that country. answer-Madagascar
-//Q2: how many hearts does an octopus have? answer-3
+
+https://media.giphy.com/media/Dv2xIMaJsisDe/giphy.gif
+
+//Q2: The cheetah is the fastest land animal; what is the second fastest? answer-antelope
+https://media.giphy.com/media/QhW73JpTi6Oic/giphy.gif
+
 //Q3: What colour are flamingos when they are born? answer-grey
+https://media.giphy.com/media/ygbGMpSxSkyo8/giphy.gif
+
 //Q4: Which animal is also known as a "Sand Rat" or "Desert Kangaroo"? answer -gerbil
+https://media.giphy.com/media/4C4Zxqs5QibUQ/giphy.gif
+
 //Q5: The horn of a rhinoceros is made of which material? answer-keratin
+https://media.giphy.com/media/8gUuSM6DgGLtYIBsOK/giphy.gif
+
 //Q6: How many knees does an elephant have? answer-4
+https://media.giphy.com/media/YSfb5vx87BpAU1P9MG/giphy.gif

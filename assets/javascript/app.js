99d3cfdde0718e624dd0e499d00bcac40cf051a4
Adam SOudani <asoudani@email.com>
2020-03-11 13:40:18 -0700

    initial site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,91 +4,108 @@ var questionAsked = [
         question: 'Lemurs are native to only one country on earth, name that country.',
         answerChoices: ['Australia', 'Madagascar', 'Mexico', 'Chad'],
         image: 'https://media.giphy.com/media/Dv2xIMaJsisDe/giphy.gif',
-        correctAnswer: 1    
+        correctAnswer: 'Madagascar'    
     },
        
     {
         question: 'The cheetah is the fastest land animal; what is the second fastest?',
         answerChoices: ['Sloth', 'Tiger', 'Antelope', ' Grizzly Bear'],
         image: 'https://media.giphy.com/media/QhW73JpTi6Oic/giphy.gif',
-        correctAnswer: 2
+        correctAnswer: 'Antelope'
     },
     
     {
         question: 'What colour are flamingos when they are born?',
         answerChoices: ['Grey', 'Pink', 'White', 'Traffic Cone Orange'],
         image: 'https://media.giphy.com/media/ygbGMpSxSkyo8/giphy.gif',    
-        correctAnswer: 0  
+        correctAnswer: 'Grey'  
     },
     
     {
         question: 'Which animal is also known as a "Sand Rat" or "Desert Kangaroo"?',
         answerChoices: ['Gerbil', 'Mongoose', 'Honey Badger', 'Polar Bear'],
         image: 'https://media.giphy.com/media/4C4Zxqs5QibUQ/giphy.gif',     
-        correctAnswer: 0
+        correctAnswer: 'Gerbil'
     },
     
     {
         question: 'The horn of a rhinoceros is made of which material?',
         answerChoices: ['Ivory', 'Gold', 'Cartilage', 'Keratin'],
         image: 'https://media.giphy.com/media/8gUuSM6DgGLtYIBsOK/giphy.gif',
-        correctAnswer: 3
+        correctAnswer: 'Keratin'
     },
         
     {
         question: 'How many knees does an elephant have?',
         answerChoices: ['Three', 'Four', 'Eight', 'Twenty'],
         image: 'https://media.giphy.com/media/YSfb5vx87BpAU1P9MG/giphy.gif',
-        correctAnswer: 1
+        correctAnswer: 'Four'
      },
        
         
 ];
 
-var answerChoices = [];
-var correctAnswer = ['Madagascar', 'Antelope', 'Grey', 'Gerbil', 'Keratin', '4'];
-var timeRemaining = '';
+var timeRemaining = 30;
 var rightAnswer = 0;
 var wrongAnswer = 0;
-var isCorrect = false;
 var userGuess;
+var currentQuestion = 0;
+var timer;
 
 
-function startGame(){
-    questionAsked = [];
-    wrongGuess = 0;
-    correctGuess = 0;
 
-    
-    var secondsRemaining = 30;
-    setInterval( function() {
-        updateStatus(secondsRemaining);
-        secondsRemaining--;
-        document.getElementById('choices_div').addEventListener('click', function(){
-            //on click check if user guess = correct answer
-            if(userGuess != correctAnswer){
-                wrongAnswer++;
-                document.createElement('div'); //add an id and text to div
-            } else if (secondsRemaining = 0){
-                wrongAnswer ++;
-                document.createElement('div');
-            }
-            else{
-                rightAnswer++;
-                var answerBox = document.createElement('div');
-                answerBox.id = 'text-box';
-                $('#text-box').text('Correct!')
-            }
-        })
+var startGame = {
+    countDown: function(){
+        timeRemaining--;
+        $('#timer').text(timeRemaining);
+        if(timeRemaining === 0){
+            startGame.endGame();
+        }
+        this.displayQuestion();
+    },
+    displayQuestion: function(){
+        timer = setInterval(this.countDown, 1000)
+        $('#question_div').html(`<h1>${questionAsked[currentQuestion].question}</h1>`)
         
-    }, 1000 );
-
-    function updateStatus(){
-        $('#timer').text(secondsRemaining)
+        for(var i = 0; i < questionAsked[currentQuestion].answerChoices.length; i++){
+            $('#choices_div').append(`<button class='select-answer' id='buttonID'></button>`)   
+        }
     }
-
-
 }
+    
+$('#startButton').on('click', function(){
+    startGame.countDown()
+})
+    
+    // var secondsRemaining = 30;
+    // setInterval( function() {
+    //     updateStatus(secondsRemaining);
+    //     secondsRemaining--;
+    //     document.getElementById('choices_div').addEventListener('click', function(){
+    //         //on click check if user guess = correct answer
+    //         if(userGuess != correctAnswer){
+    //             wrongAnswer++;
+    //             document.createElement('div'); //add an id and text to div
+    //         } else if (secondsRemaining = 0){
+    //             wrongAnswer ++;
+    //             document.createElement('div');
+    //         }
+    //         else{
+    //             rightAnswer++;
+    //             var answerBox = document.createElement('div');
+    //             answerBox.id = 'text-box';
+    //             $('#text-box').text('Correct!')
+    //         }
+    //     })
+        
+    // }, 1000 );
+
+    // function updateStatus(){
+    //     $('#timer').text(secondsRemaining)
+    // }
+
+
+
 
 
 

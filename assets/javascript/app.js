8f178269cf29bf2fd6efe98090d9db6fb29a7927
Adam SOudani <asoudani@email.com>
2020-08-25 21:23:04 -0700

    answer choice div positioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,45 +4,45 @@ var questionAsked = [
         question: 'Lemurs are native to only one country on earth, name that country.',
         answerChoices: ['Australia', 'Madagascar', 'Mexico', 'Chad'],
         image: 'https://media.giphy.com/media/Dv2xIMaJsisDe/giphy.gif',
-        correctAnswer: 'Madagascar'    
+        correctAnswer: 'Madagascar'
     },
-       
+
     {
         question: 'The cheetah is the fastest land animal; what is the second fastest?',
         answerChoices: ['Sloth', 'Tiger', 'Antelope', ' Grizzly Bear'],
         image: 'https://media.giphy.com/media/QhW73JpTi6Oic/giphy.gif',
         correctAnswer: 'Antelope'
     },
-    
+
     {
         question: 'What colour are flamingos when they are born?',
         answerChoices: ['Grey', 'Pink', 'White', 'Traffic Cone Orange'],
-        image: 'https://media.giphy.com/media/ygbGMpSxSkyo8/giphy.gif',    
-        correctAnswer: 'Grey'  
+        image: 'https://media.giphy.com/media/ygbGMpSxSkyo8/giphy.gif',
+        correctAnswer: 'Grey'
     },
-    
+
     {
         question: 'Which animal is also known as a "Sand Rat" or "Desert Kangaroo"?',
         answerChoices: ['Gerbil', 'Mongoose', 'Honey Badger', 'Polar Bear'],
-        image: 'https://media.giphy.com/media/4C4Zxqs5QibUQ/giphy.gif',     
+        image: 'https://media.giphy.com/media/4C4Zxqs5QibUQ/giphy.gif',
         correctAnswer: 'Gerbil'
     },
-    
+
     {
         question: 'The horn of a rhinoceros is made of which material?',
         answerChoices: ['Ivory', 'Gold', 'Cartilage', 'Keratin'],
         image: 'https://media.giphy.com/media/8gUuSM6DgGLtYIBsOK/giphy.gif',
         correctAnswer: 'Keratin'
     },
-        
+
     {
         question: 'How many knees does an elephant have?',
         answerChoices: ['Three', 'Four', 'Eight', 'Twenty'],
         image: 'https://media.giphy.com/media/YSfb5vx87BpAU1P9MG/giphy.gif',
         correctAnswer: 'Four'
-     },
-       
-        
+    }
+
+
 ];
 
 var timeRemaining = 30;
@@ -55,61 +55,68 @@ var timer;
 
 
 var startGame = {
-    countDown: function(){
+    countDown: function () {
         timeRemaining--;
         // $('#timer').text(timeRemaining);
         $('#timer').html('<i class="fa fa-clock-o fa-2x" aria-hidden="true"></i> ' + timeRemaining);
-        if(timeRemaining === 0){
+        if (timeRemaining === 0) {
             startGame.endGame();
             $('#timer').text('');
             $('#question_div').empty();
             $('#choices_div').empty();
             $("#answer-display").text('You\'re out of time! Correct answer is ' + questionAsked[currentQuestion].correctAnswer);
-        var elem = document.createElement('img');
-        elem.setAttribute('src', questionAsked[currentQuestion].image);
-        document.getElementById('answer-display').appendChild(elem);
-        setTimeout(startGame.displayNextQuestion, 5000); //waits 5 secs then runs displayNextQuestion function
+            var elem = document.createElement('img');
+            elem.setAttribute('src', questionAsked[currentQuestion].image);
+            document.getElementById('answer-display').appendChild(elem);
+            setTimeout(startGame.displayNextQuestion, 5000); //waits 5 secs then runs displayNextQuestion function
         }
     },
     //displays question and multiple choice answers to screen
-    displayQuestion: function(){
+    displayQuestion: function () {
         $('#answer-display').text('');
         timer = setInterval(this.countDown, 1000)
         $('#question_div').html(`<h2>${questionAsked[currentQuestion].question}</h2>`)
-        
-        for(var i = 0; i < questionAsked[currentQuestion].answerChoices.length; i++){
-            $('#choices_div').append(`<button class='select-answer' data-name = ${questionAsked[currentQuestion].answerChoices[i]}>${questionAsked[currentQuestion].answerChoices[i]}</button>`)     
+
+        for (var i = 0; i < questionAsked[currentQuestion].answerChoices.length; i++) {
+            $('#choices_div').append(`<button class='select-answer' data-name = ${questionAsked[currentQuestion].answerChoices[i]}>${questionAsked[currentQuestion].answerChoices[i]}</button>`)
         }
-        // for(var i = 0; i > questionAsked[5].correctAnswer.length; i++){
-        //     finalTally();
-        // }
+        
     },
     //reset countdown timer and move on to next question in questionAsked array
-    displayNextQuestion: function(){
-        clearInterval(timer);
-        timeRemaining = 30;
-        $('#timer').text(timeRemaining);
-        currentQuestion++;
-        startGame.displayQuestion();
+    displayNextQuestion: function () {
 
-    }, 
+        if (currentQuestion > 5) {
+            finalTally();
+        }
+        else {
+            clearInterval(timer);
+            timeRemaining = 30;
+            $('#timer').text(timeRemaining);
+            currentQuestion++;
+            startGame.displayQuestion();
+        }
+
+
+    },
     //function called in countdown function
-    endGame: function(){
+    endGame: function () {
         clearInterval(timer);
         wrongAnswer++;
     }
 
 }
-    
-$('#startButton').on('click', function(){
+
+$('#startButton').on('click', function () {
     startGame.displayQuestion();
     // this.remove('.home-screen');
     $('.home-screen').css('display', 'none')
-    $('.questions').css({'height': '500px', 'width': '600px', 
-    'background-color': 'tan', 'border-style': 'ridge', 'border-radius': '20px', 'margin': 'auto'})
+    $('.questions').css({
+        'height': '500px', 'width': '600px',
+        'background-color': 'tan', 'border-style': 'ridge', 'border-radius': '20px'
+    })
 })
-  //displays whther chosen answer choice is correct or incorrect and appends gif
-$(document).on('click', '.select-answer', function(){
+//displays whther chosen answer choice is correct or incorrect and appends gif
+$(document).on('click', '.select-answer', function () {
     // alert($(this).attr('data-name'));
     console.log(questionAsked[currentQuestion].correctAnswer);
     clearInterval(timer);
@@ -117,36 +124,40 @@ $(document).on('click', '.select-answer', function(){
     $('#question_div').empty();
     $('#choices_div').empty();
     $('#Start-game').remove();
-    if($(this).attr('data-name') === questionAsked[currentQuestion].correctAnswer){
-        console.log('correct!')
+    if ($(this).attr('data-name') === questionAsked[currentQuestion].correctAnswer) {
+        // console.log('correct!')
         $("#answer-display").text('Correct!');
         rightAnswer++;
         var elem = document.createElement('img');
         elem.setAttribute('src', questionAsked[currentQuestion].image);
         document.getElementById('answer-display').appendChild(elem);
-    } else{
-        console.log('wrong')
+    } else {
         wrongAnswer++;
-        $("#answer-display").text('Oops! Correct answer is ' + questionAsked[currentQuestion].correctAnswer);
+        console.log('Wrong answer counter: ' + wrongAnswer);
+        $("#answer-display").html('Oops! Correct answer is ' + questionAsked[currentQuestion].correctAnswer + '<br/>');
         elem = document.createElement('img');
         elem.setAttribute('src', questionAsked[currentQuestion].image);
         document.getElementById('answer-display').appendChild(elem);
     }
-   setTimeout(startGame.displayNextQuestion, 5000)
+    setTimeout(startGame.displayNextQuestion, 5000)
 });
-  
+
 
 //add function for displaying end game score tally with a button to restart game(without reloading the page)
 
-function finalTally(){
+function finalTally() {
     clearInterval(timer);
-    $('#timer').remove();
+    // $('#timer').remove();
+    $('#timer').css('display', 'none');
     $('#answer-display').empty();
-    $('#answer-display').text('Correct Answers: ' + rightAnswer);
-    $('#answer-display').text('Incorrect Answers: ' + wrongAnswer);
-    $('#answer-display').appendChild('<button class=\'restart\'>Restart Game?</button>')
+    // $('#answer-display').text('Correct Answers: ' + rightAnswer);
+    $('#answer-display').html('Correct Answers: ' + rightAnswer +
+        '<br/>' + 'Incorrect Answers: ' + wrongAnswer
+        + '<button class=\'restart\'>Restart Game?</button>');
+    // $('#answer-display').text('Incorrect Answers: ' + wrongAnswer);
+    // $('#answer-display').appendChild('<button class=\'restart\'>Restart Game?</button>')
 
-    $('.restart').on('click', function(){
+    $('.restart').on('click', function () {
         startGame.displayQuestion();
         this.remove('.restart');
     })
